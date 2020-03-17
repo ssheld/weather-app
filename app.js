8b3004ee0127f58bb38f7ac396dfe5ad7336c9ce
Stephen <20730278+ssheld@users.noreply.github.com>
2020-03-17 14:50:07 -0400

    Refactored to use object destructuring

--- app.js
+++ app.js
@@ -6,18 +6,18 @@ const address = process.argv[2];
 if (!address) {
     console.log('Please provide an address.');
 } else {
-    geocode(address, (error, data) => {
+    geocode(address, (error, {latitude, longitude, location}) => {
 
         if (error) {
             return console.log(error);
         }
     
-        forecast(data.longitude, data.latitude, (error, forecastData) => {
+        forecast(longitude, latitude, (error, forecastData) => {
     
             if (error) {
                 return console.log(error);
             }
-            console.log(data.location);
+            console.log(location);
             console.log(forecastData);
         });
     });
--- utils/forecast.js
+++ utils/forecast.js
@@ -4,16 +4,16 @@ const forecast = (longitude, latitude, callback) => {
 
     const url = 'https://api.darksky.net/forecast/' + process.env.DARKSKY_SECRET + '/' + latitude + ',' + longitude;
 
-    request({url: url, json:true}, (error, response) => {
+    request({url, json:true}, (error, {body}) => {
         if (error) {
             callback('Unable to connect to weather service.', undefined);
-        } else if (response.body.error) {
+        } else if (body.error) {
             callback('Unable to find location.', undefined);
         } else {
             callback(undefined, {
-                summary: response.body.daily.data[0].summary,
-                temperature: response.body.currently.temperature,
-                precipProbability: response.body.currently.precipProbability
+                summary: body.daily.data[0].summary,
+                temperature: body.currently.temperature,
+                precipProbability: body.currently.precipProbability
             });
         }    
     });
