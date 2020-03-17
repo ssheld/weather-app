ea74e53e8d9eb839bb61460e2964388981795aae
Stephen <20730278+ssheld@users.noreply.github.com>
2020-03-16 22:35:15 -0400

    Refactored to use callback chaining

--- app.js
+++ app.js
@@ -2,11 +2,18 @@ const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
 geocode('Boston', (error, data) => {
-    console.log('error', error);
-    console.log('data', data);
+
+    if (error) {
+        return console.log(error);
+    }
+
+    forecast(data.longitude, data.latitude, (error, forecastData) => {
+
+        if (error) {
+            return console.log(error);
+        }
+        console.log(data.location);
+        console.log(forecastData);
+    });
 });
 
-forecast(-75.7088, 44.1545, (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
-});
